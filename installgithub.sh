ac4e491f84eb51282c37a1930d05d311cec796ca
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-03-07 22:14:54 +0200

    Add files via upload

---
+++ installgithub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+archt=$(uname -m)
+link="https://github.com/bakustarver/rpgmakermlinux-cicpoffs/releases/download/v1.0.0/rpgmakerlinux-$archt-v1.0.0.tar.gz"
+basenametar=$(basename "$link")
+dirtarname=$(echo "$basenametar" | sed -e 's@.tar.gz@@g')
+wget -P "/tmp/" "$link"
+tar -xf "$defp/nwjs-$version-linux-$arch.tar.gz" -C "/tmp/"
+rm "/tmp/$basenametar"
+/tmp/$dirtarname/install.sh
