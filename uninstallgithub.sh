d276d46cb681324e05ad550717a343f534145664
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-03-09 21:09:52 +0200

    Add files via upload

---
+++ uninstallgithub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+mainfd="$HOME/desktopapps/nwjs"
+localapplicationsfd="$HOME/.local/share/applications/"
+localbin="$HOME/.local/bin"
+
+
+rm -rf "$mainfd"
+rm "$localapplicationsfd/nwjstest.desktop"
+rm "$localbin/rpgmaker-linux"
