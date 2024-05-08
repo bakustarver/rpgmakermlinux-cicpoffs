13d6340cdccc11fb3edfa8b3420695532c536bdc
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-05-09 02:37:22 +0300

    Add files via upload

--- nwjs/dwnwjs.sh
+++ nwjs/dwnwjs.sh
@@ -2,21 +2,50 @@
 
 gitlink="https://github.com/nwjs/nw.js"
 # defp="$HOME/desktopapps/nwjs/nwjs"
-defp=$(dirname "$0")
+if [ -z "$defpn" ]; then
+defpn=$(dirname "$0")
+else
+defp="$defpn"
+fi
+# kdialog --msgbox "$test $depfp"
 # defp="$defp0/nwjs"
-echo "defp $defp"
+# echo "defp $defp"
 if ! [ -d "$defp" ]; then
 mkdir -p "$defp"
 fi;
+
+nwjslist=$(ls -p "$defp/nwjs" | grep /)
+
 nwjsfm="$HOME/desktopapps/nwjs/nwjs"
 
 if [ -f "$nwjsfm/packagefiles/usesdk.txt" ]; then
 SDKNWJS=true
+nwjslist=$(echo "$nwjslist" | grep "sdk")
+else
+nwjslist=$(echo "$nwjslist" | grep -v "sdk")
 fi
 echo "$SDKNWJS"
 versioninstalledlist=$(ls "$defp")
 arch=$(uname -m | sed -e 's@i686@ia32@g' -e 's@x86_64@x64@g' -e 's@armv7l@armhf@g' -e 's@armhf@arm@g' -e 's@aarch64@arm64@g')
 
+
+downloadversion() {
+if [ -n "$SDKNWJS" ]; then
+# rm "$defp/nwjs-$version-linux-$arch.tar.gz"
+
+wget -c -P "$defp" https://dl.nwjs.io/$version/nwjs-sdk-$version-linux-$arch.tar.gz
+
+tar -xf "$defp/nwjs-sdk-$version-linux-$arch.tar.gz" -C "$defp/nwjs"
+rm "$defp/nwjs-sdk-$version-linux-$arch.tar.gz"
+else
+# rm "$defp/nwjs-$version-linux-$arch.tar.gz"
+wget -c -P "$defp" https://dl.nwjs.io/$version/nwjs-$version-linux-$arch.tar.gz
+
+
+tar -xf "$defp/nwjs-$version-linux-$arch.tar.gz" -C "$defp/nwjs"
+rm "$defp/nwjs-$version-linux-$arch.tar.gz"
+fi
+}
 
 
 downloadandextract() {
@@ -52,6 +81,7 @@ if [ "$arch" = "x64" ] || [ "$arch" = "ia32" ]; then
 
 if [ -z "$@" ]; then
 version=$(wget --user-agent 'Mozilla/5.0 (Windows NT 10.0; rv:124.0) Gecko/20100101 Firefox/124.0' -qO- "https://github.com/nwjs/nw.js/tags" | grep 'Link--primary Link' | head -n 1 | sed -e 's@.*">@@g' -e 's@<.*@@g' -e 's@nw-@@g')
+echo "latest version is $version"
 else
 if echo "$@" | grep -q "v[0-1].[0-9][0-9].[0]"; then
 version="$@"
@@ -68,7 +98,7 @@ fi
 
 
 # echo "$latestlocal $version"
-if [ -n "$latestlocal" ] && [ "$latestlocal" = "$version" ]; then
+if [ -n "$latestlocal" ] && [ "$latestlocal" = "$version" ] && [ -z "$checkversionnwjs" ]; then
 echo Your nwjs version is latest
 echo Reinstall? y/n
 read
@@ -83,30 +113,25 @@ else
 exit;
 fi
 fi
-# kdialog --msgbox "hello 2"
 
 
 # uname -p
-echo "sdk q$SDKNWJS"
+# echo "sdk q$SDKNWJS"
 
 if [ "$skipdownloadifexist" = "true" ]; then
-
-echo skipping download;
+# if  "$nwjslist"
+if [ -n "$nwjslist" ] && echo "$nwjslist" | grep -q "$version" ; then
+# version
+echo skiping
 else
-if [ -n "$SDKNWJS" ]; then
-wget -P "$defp" https://dl.nwjs.io/$version/nwjs-sdk-$version-linux-$arch.tar.gz
-
-tar -xf "$defp/nwjs-sdk-$version-linux-$arch.tar.gz" -C "$defp/nwjs"
-rm "$defp/nwjs-sdk-$version-linux-$arch.tar.gz"
-else
-wget -P "$defp" https://dl.nwjs.io/$version/nwjs-$version-linux-$arch.tar.gz
-
-
-tar -xf "$defp/nwjs-$version-linux-$arch.tar.gz" -C "$defp/nwjs"
-rm "$defp/nwjs-$version-linux-$arch.tar.gz"
+downloadversion
 fi
+# echo skipping download;
+else
+downloadversion
 fi
 echo Finished
 fi
-fi
+# fi
 
+sleep 1;
