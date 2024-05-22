4e30bf2652ccf51436ecdabf28e57aa5dc68e700
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-05-22 14:32:29 +0300

    Update uninstallgithub.sh

--- uninstallgithub.sh
+++ uninstallgithub.sh
@@ -12,3 +12,5 @@ rm "$localapplicationsfd/nwjsoptions.desktop"
 rm "$localbin/rpgmaker-linux"
 rm -rf "$HOME/.local/share/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/"
 rm -rf "$HOME/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/"
+
+sed -e 's@x-scheme-handler/rpgmakermp.*@@g' -i /home/pasha/.config/mimeapps.list
