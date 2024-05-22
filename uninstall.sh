b3504819bb848073adaae066f855ff8d04afd84f
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-05-22 14:28:05 +0300

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 
-rm -rf "$HOME/desktopapps/nwjs" && rm "$HOME/.local/share/applications/nwjstest.desktop" && rm "$HOME/.local/bin/rpgmaker-linux"
+
+
+rm -rf "$HOME/desktopapps/nwjs" && rm "$HOME/.local/share/applications/nwjstest.desktop" && rm "$HOME/.local/share/applications/nwjsoptions.desktop" && rm "$HOME/.local/bin/rpgmaker-linux" && rm -rf "$HOME/.steam/steam/compatibilitytools.d/rpgmaker-linux-cicpoffs"
+sed -e 's@x-scheme-handler/rpgmakermp.*@@g' -i /home/pasha/.config/mimeapps.list
