94c14c5426ea573c3239120a634698a00139fd07
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-05-23 06:21:29 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,4 +4,4 @@
 
 
 rm -rf "$HOME/desktopapps/nwjs" && rm "$HOME/.local/share/applications/nwjstest.desktop" && rm "$HOME/.local/share/applications/nwjsoptions.desktop" && rm "$HOME/.local/bin/rpgmaker-linux" && rm -rf "$HOME/.local/share/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/" && rm -rf "$HOME/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/"
-sed -e 's@x-scheme-handler/rpgmakermp.*@@g' -i /home/pasha/.config/mimeapps.list
+sed -e 's@x-scheme-handler/rpgmakermp.*@@g' -i "$HOME/.config/mimeapps.list"
