3c147c8fced37bc5a5677ae87ae7d45303eae67f
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-10-26 04:28:31 +0300

    Update installgithub.sh

--- installgithub.sh
+++ installgithub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 archt=$(uname -m | sed -e 's/armv7l/armhf/g')
-version=1.1.2
+version=1.1.3
 link="https://github.com/bakustarver/rpgmakermlinux-cicpoffs/releases/download/v$version/rpgmakerlinux-$archt-v$version.tar.gz"
 basenametar=$(basename "$link")
 dirtarname=$(echo "$basenametar" | sed -e 's@.tar.gz@@g')
