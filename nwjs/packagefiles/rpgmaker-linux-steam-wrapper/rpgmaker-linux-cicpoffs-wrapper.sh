a2ffabdb6b6c6e0d189d9d88b88f66da340c4195
bakustarver <66978329+bakustarver@users.noreply.github.com>
2024-11-18 19:03:37 +0200

    Update rpgmaker-linux-cicpoffs-wrapper.sh

--- nwjs/packagefiles/rpgmaker-linux-steam-wrapper/rpgmaker-linux-cicpoffs-wrapper.sh
+++ nwjs/packagefiles/rpgmaker-linux-steam-wrapper/rpgmaker-linux-cicpoffs-wrapper.sh
@@ -7,21 +7,29 @@ if [[ ! -z "${LD_PRELOAD}" ]]; then
 #      export PATH=""
 fi
 export LD_LIBRARY_PATH=""
-
-#
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ $2 == *"iscriptevaluator.exe"* ]]; then
   echo "ignoring iscriptevaluator.exe"
   exit 0
 fi
-
 if [ -z "$SteamAppId" ]; then
-  echo "no steam app id"
+  echo "no steam app id, exiting"
   exit 0
 fi
+
+
 export LC_ALL=""
 startset=$(echo "$@" | sed -e 's@waitforexitandrun @@g' -e 's@ --in-process-gpu@@g')
 export gamef=$(dirname "$startset")
 
+# zenity --error --text "$gamef"
+
+if [ -f "$HOME/desktopapps/nwjs/nwjs/packagefiles/nwjsstart-cicpoffs.sh" ]; then
+$HOME/desktopapps/nwjs/nwjs/packagefiles/nwjsstart-cicpoffs.sh --gui
+else
+##steam's xgd-open stopped working, using default
+NEWPATH=$(echo "$PATH" | tr ':' '\n' | grep -v "steam-run" | tr '\n' ':')
+export PATH=$NEWPATH
 xdg-open "rpgmakermp:///$gamef"
+fi
