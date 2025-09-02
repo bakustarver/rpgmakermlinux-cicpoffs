d671683087ef0e32fdfa7c3d0ea1d516b0c72527
bakustarver <66978329+bakustarver@users.noreply.github.com>
2025-09-02 10:51:42 +0300

    Update uninstallgithub.sh

--- uninstallgithub.sh
+++ uninstallgithub.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 
-mainfd="$HOME/desktopapps/nwjs"
-localapplicationsfd="$HOME/.local/share/applications/"
-localbin="$HOME/.local/bin"
 
-
-rm -rf "$mainfd"
-rm "$localapplicationsfd/nwjstest.desktop"
-rm "$localapplicationsfd/nwjsoptions.desktop"
-rm "$localbin/rpgmaker-linux"
-rm -rf "$HOME/.local/share/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/"
+mainfdf="$HOME/.config/defrpgmakerlinuxpath.txt"
+if [ -f "$mainfdf" ] && [ -s "$mainfdf" ]; then
+mainfd=$(cat "$mainfdf")
+if [ -d "$mainfd" ] && [ -n "$mainfd" ]; then
+rm -rf "$mainfd/desktopapps/";
+fi
+else
+rm -rf "$HOME/desktopapps/";
+fi
+rm "$HOME/.local/share/applications/nwjstest.desktop";
+rm "$HOME/.local/share/applications/nwjsoptions.desktop";
+rm "$HOME/.local/bin/rpgmaker-linux";
+rm -rf "$HOME/.local/share/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/";
 rm -rf "$HOME/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/"
-
 sed -e 's@x-scheme-handler/rpgmakermp.*@@g' -i "$HOME/.config/mimeapps.list"
