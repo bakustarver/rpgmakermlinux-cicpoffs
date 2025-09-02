81bdeda69548614366c1e6cbce104aa5c0b16543
bakustarver <66978329+bakustarver@users.noreply.github.com>
2025-09-02 10:50:57 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,15 @@
 
 
 
-rm -rf "$HOME/desktopapps/nwjs";
-rm "$HOME/.local/share/applications/nwjstest.desktop";
+mainfdf="$HOME/.config/defrpgmakerlinuxpath.txt"
+if [ -f "$mainfdf" ] && [ -s "$mainfdf" ]; then
+mainfd=$(cat "$mainfdf")
+if [ -d "$mainfd" ] && [ -n "$mainfd" ]; then
+rm -rf "$mainfd/desktopapps/";
+fi
+else
+rm -rf "$HOME/desktopapps/";
+firm "$HOME/.local/share/applications/nwjstest.desktop";
 rm "$HOME/.local/share/applications/nwjsoptions.desktop";
 rm "$HOME/.local/bin/rpgmaker-linux";
 rm -rf "$HOME/.local/share/Steam/compatibilitytools.d/rpgmaker-linux-steam-wrapper/";
